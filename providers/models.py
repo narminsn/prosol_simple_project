4fa019c4dc4f9c698c471f3718e61e4e8ce03481
narminsn <nsultanzadehh@gmail.com>
2020-12-23 09:47:55 +0400

    forms-change

--- providers/models.py
+++ providers/models.py
@@ -41,9 +41,9 @@ class ProviderContact(models.Model):
     provider = models.OneToOneField('Provider',on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    rol = models.CharField(max_length=100, null=True)
+    rol = models.CharField(max_length=100, null=True,blank=True)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100,null=True,blank=True)
     mob = models.CharField(max_length=100)
 
     def __str__(self):
