051d21b172201e4f8410f569df1c07063e3f4e7a
narminsn <nsultanzadehh@gmail.com>
2020-12-23 10:32:09 +0400

    admin-confirm

--- providers/admin.py
+++ providers/admin.py
@@ -29,13 +29,25 @@ class ServiceInline(admin.TabularInline):
 
 @admin.register(models.Provider)
 class ProviderAdmin(admin.ModelAdmin):
-    
-    inlines = (ContactInline,ServiceInline,SocialInline )
+
+    def provider_contact_name(self, obj):
+        return f'{obj.providercontact.name} {obj.providercontact.last_name}'
+
+    def provider_contact_phone(self, obj):
+        return obj.providercontact.phone
+
+    def service_status(self, obj):
+        return obj.serviceinformation.get_status_display()
+
+    def service_name(self, obj):
+        return obj.serviceinformation.get_service_type_display()
+
+    list_display = ['public_name','service_name','provider_contact_name','provider_contact_phone','service_status']
+    inlines = (SocialInline,ContactInline,ServiceInline )
     change_form_template = 'admin/change_form.html'
     def get_readonly_fields(self, request, obj=None):
         if obj:
             self.readonly_fields = [field.name for field in obj.__class__._meta.fields]
         return self.readonly_fields
 
-    # readonly_fields = get_readonly_fields()
 
